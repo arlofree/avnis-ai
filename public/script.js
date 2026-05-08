4355ddcd0f7739736019864637bcba53ac2bde49
arlofree <147103681+arlofree@users.noreply.github.com>
2026-05-08 21:22:57 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,95 @@
+// script.js
+
+async function sendMessage() {
+
+  const name =
+    document.getElementById("name").value;
+
+  const dob =
+    document.getElementById("dob").value;
+
+  const tob =
+    document.getElementById("tob").value;
+
+  const lat =
+    document.getElementById("lat").value;
+
+  const lon =
+    document.getElementById("lon").value;
+
+  const question =
+    document.getElementById("question").value;
+
+  const mode =
+    document.getElementById("mode").value;
+
+  const chatbox =
+    document.getElementById("chatbox");
+
+
+
+  chatbox.innerHTML =
+    "Loading Astrology Reading...";
+
+
+
+  try {
+
+    const response =
+      await fetch(
+
+        "http://localhost:3000/chat",
+
+        {
+
+          method: "POST",
+
+          headers: {
+            "Content-Type":
+              "application/json"
+          },
+
+          body: JSON.stringify({
+
+            name,
+            dob,
+            tob,
+            lat,
+            lon,
+            question,
+            mode
+
+          })
+
+        }
+
+      );
+
+
+
+    const data =
+      await response.json();
+
+
+
+    chatbox.innerHTML = `
+
+      <pre>${data.reply}</pre>
+
+    `;
+
+  }
+
+  catch (error) {
+
+    console.log(error);
+
+    chatbox.innerHTML = `
+
+❌ Server Not Responding
+
+`;
+
+  }
+
+}
